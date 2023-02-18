b8b3ef2e1274746a74469888de1c527af21b1166
Marc Blais <marcantblais@gmail.com>
2023-02-18 12:05:04 -0500

    added echo off

--- devserver.bat
+++ devserver.bat
@@ -1,3 +1,4 @@
+@echo off
 netsh interface portproxy reset
 FOR /F "delims=" %%i IN ('wsl hostname -I') DO set wslIP=%%i
 netsh interface portproxy add v4tov4 listenport=3000 connectport=3000 listenaddress=0.0.0.0 connectaddress=%wslIP%
